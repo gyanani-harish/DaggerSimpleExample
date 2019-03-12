35bdf1240dbc84c6c7d67519d7bd3d96f6638d78
Harish Gyanani <gyanani.harish@gmail.com>
2019-03-12 12:11:19 +0530

    Create GsonModule.java

---
+++ app/src/main/java/harish.mysimpledagger/modules/GsonModule.java
@@ -0,0 +1,30 @@
+package harish.mysimpledagger.modules;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class GsonModule {
+
+    String mBaseUrl;
+
+    public GsonModule(String baseUrl) {
+        this.mBaseUrl = baseUrl;
+    }
+
+
+    @Provides  // Dagger will only look for methods annotated with @Provides
+    @Singleton
+    Gson provideGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+        return gsonBuilder.create();
+    }
+
+}
